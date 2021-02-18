ee5be84e6df500577cfe3d2413d9c40c234d1403
StarTux <ths.loy@googlemail.com>
2021-02-19 00:33:24 +0100

    Add missing files

---
+++ src/main/java/com/cavetale/home/util/Colors.java
@@ -0,0 +1,9 @@
+package com.cavetale.home.util;
+
+import net.md_5.bungee.api.ChatColor;
+
+public final class Colors {
+    private Colors() { }
+
+    public static final ChatColor RED = ChatColor.of("#FF0000");
+}

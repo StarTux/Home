80bfc2632708639741b9036d1fca7aaf4afe4e44
StarTux <ths.loy@gmail.com>
2024-07-27 10:35:42 +0200

    Disable the test case for now

--- src/test/java/com/cavetale/home/sql/SQLTest.java
+++ src/test/java/com/cavetale/home/sql/SQLTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import static com.winthier.sql.SQLDatabase.testTableCreation;
 
 public final class SQLTest {
-    @Test
     public void test() {
         for (var it : SQLStatic.getAllTableClasses()) {
             System.out.println(testTableCreation(it));
